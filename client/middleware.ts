6cc6bcd8c9ab1fa0c4ec77c0bff43eb75883d6e4
Syed Mohsin Ali Shah <shah.syedmohsin1@gmail.com>
2026-02-01 16:09:44 +0100

    pdf functionality updated with updations

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api/script-templates(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
